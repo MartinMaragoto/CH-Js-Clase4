d2181190d073128bceafdef9b4d0be3cde015552
Martin Maragoto <79815000+MartinMaragoto@users.noreply.github.com>
2023-08-09 16:22:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//NAVBAR----------------------------------------------------------
+// const navBarElement = document.getElementById ('navbar')
+const navbarLi = document.querySelectorAll ('#navbar li')
+
+// const burguerNavbarElement = document.getElementById ('burguerNavbar')
+// const burguerLi = document.querySelectorAll ('#burguerNavbar li')
+
+const iconNavbarElement = document.getElementById ('iconNavbar')
+const iconNavbarLi = document.querySelectorAll ('#iconNavbar li')
+
+
+
+
+// function loadingPage() {
+//     navBarElement.style.display = 'none'
+//     navbarLi.forEach(li => li.style.display = 'none')
+//     burguerNavbarElement.style.display = 'none'
+// }
+
+// window.addEventListener ('load', loadingPage)
